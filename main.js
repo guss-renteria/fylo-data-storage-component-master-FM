9b6048e16c1f54b534bed86f2f9fb4ee03f02147
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-06-16 21:40:32 -0500

    se configuro el cambio de backgrounds

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ import './normalize.css'
 import './style.scss'
 import './icons/icomoon/style.css'
 
-const app = document.querySelector('#app')
+import bg_mobile from './images/bg-mobile.png'
+import bg_desktop from './images/bg-desktop.png'
+
+const background = document.querySelector('div.background')
+const is_mobile = undefined;
+
+const resize = () => {
+  if(window.innerWidth > 992 && (is_mobile == undefined || is_mobile)) {
+    background.style.backgroundImage = `url(${ bg_desktop })`
+  }else if(window.innerWidth <= 992 && (is_mobile == undefined || !is_mobile)) {
+    background.style.backgroundImage = `url(${ bg_mobile })`
+  }
+}
+
+resize()
+//
+window.onresize = resize
